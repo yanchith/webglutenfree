73916f718f0fc54bd54582170d639571ddb1fc33
yanchith <yanchi.toth@gmail.com>
2017-11-10 19:50:10 +0100

    Remove dist

--- dist/assert.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function isTrue(condition: boolean, message?: string): void;
-export declare function never(x: never, message?: string): never;
--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { RenderPass, RenderPassProps } from "./render-pass";
-export { VertexBuffer, VertexBufferProps, VertexBufferType } from "./vertex-buffer";
-export { ElementBuffer, ElementBufferProps, ElementPrimitive } from "./element-buffer";
-export { VertexArray, VertexArrayProps } from "./vertex-array";
-export { Texture, TextureOptions, TextureType, TextureFilter, TextureWrap, TextureInternalFormat, TextureFormat } from "./texture";
-export { Framebuffer } from "./framebuffer";
