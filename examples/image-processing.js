7536e1eafff94095ddd1bbe7101a444af06403b3
yanchith <yanchi.toth@gmail.com>
2017-11-13 19:20:41 +0100

    Simplify framebuffer example

--- examples/image-processing.js
+++ examples/image-processing.js
@@ -188,14 +188,6 @@ async function run() {
         ],
     });
 
-    gl.clearColor(0, 0, 0, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    fbo.bind();
-    gl.clearColor(0, 0, 0, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    fbo.unbind();
-
     texturePass.renderToFramebuffer(texturePassGeometry, undefined, fbo);
     kernelPass.render(kernelPassGeometry, undefined);
 }
