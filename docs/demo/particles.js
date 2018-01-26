e407c9ec7dcda784b1cd7a1c439e6cfbc654b280
yanchith <yanchi.toth@gmail.com>
2018-01-26 18:21:45 +0100

    Upgrade particle demo to use trianges

--- docs/demo/particles.js
+++ docs/demo/particles.js
@@ -1,6 +1,7 @@
 import {
     Device,
     Command,
+    BlendFunc,
     VertexBuffer,
     VertexBufferType,
     AttributeData,
@@ -9,12 +10,14 @@ import {
 } from "./lib/glutenfree.es.js";
 
 const N_PARTICLES = 10000;
-const WANDER_FACTOR = 0.005;
+const WANDER_FACTOR = 0.1;
+const SCALE = 800;
 
 const dev = Device.mount();
 const [width, height] = [dev.bufferWidth, dev.bufferHeight];
 
 const view = mat4.create();
+let flip = 0;
 
 const cmd = Command.create(
     dev,
@@ -22,15 +25,18 @@ const cmd = Command.create(
         precision mediump float;
 
         uniform mat4 u_projection, u_model, u_view;
+        uniform float u_flip;
 
         layout (location = 0) in vec3 a_position;
+        layout (location = 1) in vec3 a_local_orig;
+        layout (location = 2) in vec3 a_local_flip;
 
         void main() {
-            gl_PointSize = 2.0;
+            vec3 local = a_local_orig + (a_local_flip - a_local_orig) * u_flip;
             gl_Position = u_projection
                 * u_view
                 * u_model
-                * vec4(a_position, 1.0);
+                * vec4(a_position + local, 1.0);
         }
     `,
     `#version 300 es
@@ -39,7 +45,7 @@ const cmd = Command.create(
         out vec4 f_color;
 
         void main() {
-            f_color = vec4(1.0, 0.1, 0.3, 1.0);
+            f_color = vec4(0.9, 0.9, 1.0, 0.5);
         }
     `,
     {
@@ -62,10 +68,20 @@ const cmd = Command.create(
                 type: "matrix4fv",
                 value: time => mat4.lookAt(
                     view,
-                    [4 * Math.cos(time / 10000), 1, 4 * Math.sin(time / 10000)],
+                    [SCALE * Math.cos(time / 10000), 1, SCALE * Math.sin(time / 10000)],
                     [0, 0, 0],
                     [0, 1, 0],
                 ),
+            },
+            u_flip: {
+                type: "1f",
+                value: () => flip++ % 2,
+            }
+        },
+        blend: {
+            func: {
+                src: BlendFunc.SRC_ALPHA,
+                dst: BlendFunc.ONE_MINUS_SRC_ALPHA,
             },
         },
     },
@@ -74,7 +90,7 @@ const cmd = Command.create(
 const halfWander = WANDER_FACTOR / 2;
 const particlePositions = new Float32Array(N_PARTICLES * 3);
 for (let i = 0; i < particlePositions.length; i++) {
-    particlePositions[i] = Math.random() * 2 - 1;
+    particlePositions[i] = Math.random() * SCALE - SCALE / 2;
 }
 
 const buffer = VertexBuffer.create(
@@ -84,16 +100,29 @@ const buffer = VertexBuffer.create(
     BufferUsage.DYNAMIC_DRAW,
 );
 
+const angle = Math.PI * 2 / 3;
 const attrs = AttributeData.create(
     dev,
-    Primitive.POINTS,
+    Primitive.TRIANGLES,
     cmd.locate({
+        // a_position is an instanced buffer containing the particles center
         a_position: {
             type: "pointer",
             buffer,
             count: particlePositions.length / 3,  // 3 components per item
             size: 3,  // 3 components per item
+            divisor: 1,
         },
+        a_local_orig: [
+            [Math.sin(angle * 2 + Math.PI), Math.cos(angle * 2 + Math.PI), 0],
+            [Math.sin(angle + Math.PI), Math.cos(angle + Math.PI), 0],
+            [Math.sin(angle * 3 + Math.PI), Math.cos(angle * 3 + Math.PI), 0],
+        ],
+        a_local_flip: [
+            [Math.sin(angle * 2), Math.cos(angle * 2), 0],
+            [Math.sin(angle), Math.cos(angle), 0],
+            [Math.sin(angle * 3), Math.cos(angle * 3), 0],
+        ]
     }),
 );
 
