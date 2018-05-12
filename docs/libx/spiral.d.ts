82d1009dab5dfe637e572e9b200594429ac394d0
yanchith <yanchi.toth@gmail.com>
2018-05-12 23:47:37 +1000

    Fix spiral.d.ts

--- docs/libx/spiral.d.ts
+++ docs/libx/spiral.d.ts
@@ -1,3 +1,3 @@
 export declare const positions: [number, number, number][];
 export declare const normals: [number, number, number][];
-export declare const elements: [number, number, number][];
+export declare const uvs: [number, number][];
